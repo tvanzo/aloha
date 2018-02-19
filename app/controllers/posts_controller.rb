72148581c7412eaf4f39e2908ce4a056c68c0edc
EC2 Default User <ec2-user@ip-172-31-13-141.us-east-2.compute.internal>
2018-02-19 05:19:20 +0000

    through lesson 5

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+  def index
+    @posts=Post.all.order(created_at: :desc)
+    @post=Post.new
+  end
+  
+  def new
+     @post = Post.new
+     
+  end
+  
+  def show
+    @post=Post.find_by(id:params[:id])
+  end
+  
+  def create
+    @post = Post.new(content: params[:content])
+    if @post.save
+      flash[:notice] = "Post successfully created"
+      redirect_to("/posts/index")
+    else
+      render("posts/new")
+    end
+  end
+  
+  def destroy
+  @post=Post.find_by(id:params[:id])
+    @post.destroy
+    flash[:notice] = "Post successfully deleted"
+    redirect_to("/posts")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to =>"home#index"
+  get 'posts' =>"posts#index"
+  get 'posts/new' =>"posts#new"
+  post 'posts/create' =>"posts#create"
+  get 'posts/:id' =>"posts#show"
+  post "posts/:id/destroy" => "posts#destroy"
 end
