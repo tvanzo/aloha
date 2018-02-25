b3d66443078844bbac9b4f1b2520c943ac2417d3
EC2 Default User <ec2-user@ip-172-31-34-133.us-east-2.compute.internal>
2018-02-25 13:51:24 +0000

    goog auth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180224000048) do
+ActiveRecord::Schema.define(version: 20180225134759) do
 
   create_table "answers", force: :cascade do |t|
     t.datetime "created_at", null: false
