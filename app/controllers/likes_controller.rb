83cddf187ec47bc5e6f04a6caba05020010226c3
EC2 Default User <ec2-user@ip-172-31-34-133.us-east-2.compute.internal>
2018-03-06 08:06:12 +0000

    last

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -14,7 +14,7 @@ class LikesController < ApplicationController
   def destroy
     @like=Like.find_by(user_id:current_user.id, post_id:params[:post_id])
     @like.destroy
-    redirect_to("/posts/#{params[:post_id]}")
+    redirect_to request.referrer
   end
     
 end
